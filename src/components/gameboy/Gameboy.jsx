163ac23c3d38aa56fcc7be8d4a3e9361a92579f8
Franco León Cáceres <francocaceres008@gmail.com>
2023-08-31 20:35:02 -0300

    feat(components/views):added landing page Styles

--- src/components/gameboy/Gameboy.jsx
+++ src/components/gameboy/Gameboy.jsx
@@ -1,4 +1,5 @@
 import LandingPage from "../.././views/landingPage/LandingPage.jsx";
+import { CgZeit } from "react-icons/cg";
 import "./_Gameboy.module.scss";
 
 const Gameboy = () => {
@@ -14,28 +15,51 @@ const Gameboy = () => {
           <div className="controllers-1">
             <div className="row-buttons">
               <div className="side-buttons">
-                <button className="button-left"></button>
+                <button className="button-left">
+                  <CgZeit className="button-img left" />
+                </button>
               </div>
               <div className="center-buttons">
-                <button className="button-up"></button>
-                <span className="empty-center"></span>
-                <button className="button-down"></button>
+                <button className="button-up">
+                  <CgZeit className="button-img up" />
+                </button>
+                <span className="empty-center">
+                  <div className="inner-square top-left"></div>
+                  <div className="inner-square top-right"></div>
+                  <div className="inner-square bottom-left"></div>
+                  <div className="inner-square bottom-right"></div>
+                </span>
+                <button className="button-down">
+                  <CgZeit className="button-img down" />
+                </button>
               </div>
               <div className="side-buttons">
-                <button className="button-right"></button>
+                <button className="button-right">
+                  <CgZeit className="button-img right" />
+                </button>
               </div>
             </div>
           </div>
           <div className="controllers-2">
             <div className="center-buttons-2">
-              <button className="buttons-center"></button>
-              <button className="buttons-center"></button>
+              <span className="select">
+                <button className="buttons-center"></button>
+                <span className="select-text">SELECT</span>
+              </span>
+              <span className="start">
+                <button className="buttons-center"></button>
+                <span className="start-text">START</span>
+              </span>
             </div>
           </div>
           <div className="controllers-3">
             <div className="circle-button-container">
-              <button className="circle-button-1"></button>
-              <button className="circle-button-2"></button>
+              <div className="circle-button-container-1">
+                <button className="circle-button-1">B</button>
+              </div>
+              <div className="circle-button-container-2">
+                <button className="circle-button-2">A</button>
+              </div>
             </div>
           </div>
         </div>
