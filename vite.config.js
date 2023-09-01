b831fc5bbacc350d01531064bab2d971e2be4123
Franco León Cáceres <francocaceres008@gmail.com>
2023-09-01 20:10:39 -0300

    fixed bugs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import ViteSassPlugin from "vite-plugin-sass"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import ViteSassPlugin from "vite-plugin-sass";
 
 export default defineConfig({
-  plugins: [react(), ViteSassPlugin()],
+  plugins: [
+    react(),
+    ViteSassPlugin({
+      sass: {
+        include: ["src/components/**/*.scss", "src/views/**/*.scss"],
+      },
+    }),
+  ],
 });
